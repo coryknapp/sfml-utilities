6d2ffe1890f761fc3e4c58ef55782d28c5ee705a
Cory Knapp <knapp.cory@gmail.com>
2015-07-01 14:33:18 -0700

    Added SimpleRectangle struct.

--- sfml-utilities.hpp
+++ sfml-utilities.hpp
@@ -9,18 +9,52 @@ using namespace sf;
 namespace sfu{
 
 template< typename IN, typename OUT >
-Vector2<OUT> convertVector2( const Vector2<IN> &vec ) {
-	return Vector2<OUT>( (IN)vec.x, (IN)vec.y ); 
+static inline Vector2<OUT> convertVector2( const Vector2<IN> &vec ) {
+	return Vector2<OUT>( (IN)vec.x, (IN)vec.y );
 }
+	
+// some new types that can be useful
+// For example, I made this while unaware of sf::Rect.
+template<typename T>
+struct SimpleRectangle{
 
-RectangleShape getRectForCorners( Vector2f one, Vector2f two ){
+	Vector2<T> position;
+	Vector2<T> size;
+
+	SimpleRectangle() = default;
+	SimpleRectangle( const RectangleShape &rect ){
+		position = convertVector2<float, T>(rect.getPosition());
+		size = convertVector2<float, T>(rect.getSize());
+	}
+	SimpleRectangle( const Rect<T> &rect ){
+		position.x = rect.left;
+		position.y = rect.top;
+		size.x = rect.width;
+		size.y = rect.height;
+	}
+	operator RectangleShape(){
+		RectangleShape rect;
+		rect.setPosition( convertVector2<T, float>( position ) );
+		rect.setSize( convertVector2<T, float>( size ) );
+		return rect;
+	}
+	operator Rect<T>(){
+		RectangleShape rect;
+		rect.setPosition( convertVector2<T, float>( position ) );
+		rect.setSize( convertVector2<T, float>( size ) );
+		return rect;
+	}
+};
+
+template< typename T>
+static inline RectangleShape getRectForCorners( Vector2<T> one, Vector2<T> two ){
 	RectangleShape rect;
 	rect.setPosition( std::min( one.x, two.x), std::min( one.y, two.y ) );
 	rect.setSize( Vector2f( fabs( one.x - two.x ), fabs( one.y - two.y ) ) );
 	return rect;
 }
 
-char quadrant( sf::Vector2f v ){
+static inline char quadrant( sf::Vector2f v ){
 	if( v.x > 0 ){ // I or VI
 		if( v.y > 0 )
 			return 1; // I
@@ -32,7 +66,7 @@ char quadrant( sf::Vector2f v ){
 	}
 }
 
-float angleOff0( sf::Vector2f v ){
+static inline float angleOff0( sf::Vector2f v ){
 	float atan = atanf( v.y / v.x );
 	
 	auto quad = quadrant( v );
@@ -45,16 +79,16 @@ float angleOff0( sf::Vector2f v ){
 	return atan;
 }
 
-float angleBetween( sf::Vector2f a, sf::Vector2f b ){
+static inline float angleBetween( sf::Vector2f a, sf::Vector2f b ){
 	return angleOff0( b ) - angleOff0( a );
 }
 
-sf::Vector2f vectorForAngleFrom0( float rads ){
+static inline sf::Vector2f vectorForAngleFrom0( float rads ){
 	return sf::Vector2f( cosf( rads), sinf( rads ) );
 }
 
 template <typename T>
-float distance( const sf::Vector2<T> &a, const sf::Vector2<T> &b ){
+static inline float distance( const sf::Vector2<T> &a, const sf::Vector2<T> &b ){
 	return sqrt( pow( a.x - b.x, 2) + pow( a.y - b.y, 2 ) );
 }
 
