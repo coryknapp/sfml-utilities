4f624a40ab2d49069e1338099e0c5568ec8f24c0
Cory Knapp <knapp.cory@gmail.com>
2015-07-06 19:39:40 -0700

    added tests for intersection.

--- test.cpp
+++ test.cpp
@@ -6,7 +6,53 @@
 using namespace sfu;
 using namespace sf;
 
-TEST_CASE( "SimplePolygon checkConvex", "[simplepolygon]" ) {
+
+TEST_CASE( "SimpleRectangle", "[simplerectangle]" ) {
+
+	SimpleRectangle<int> one;
+	SimpleRectangle<int> two;
+	SECTION( "no intersection" ) {	
+		one.position = Vector2i( 0, 0 );
+		two.position = Vector2i( 4, 0 );
+		one.size = Vector2i( 2, 2 );
+		two.size = Vector2i( 2, 2 );
+
+		REQUIRE( intersection(one, two).size.x == 0 );		
+		REQUIRE( intersection(one, two).size.y == 0 );		
+	}
+
+	SECTION( "asymmetrical intersection" ) {	
+		one.position = Vector2i( 0, 0 );
+		two.position = Vector2i( 1, 1 );
+		one.size = Vector2i( 2, 2 );
+		two.size = Vector2i( 2, 2 );
+
+		REQUIRE( intersection(one, two).size.x == 1 );		
+		REQUIRE( intersection(one, two).size.y == 1 );		
+		REQUIRE( intersection(one, two).position.x == 1 );		
+		REQUIRE( intersection(one, two).position.y == 1 );
+		
+		REQUIRE( intersection(one, two).size.x == 1 );		
+		REQUIRE( intersection(one, two).size.y == 1 );		
+		REQUIRE( intersection(one, two).position.x == 1 );		
+		REQUIRE( intersection(one, two).position.y == 1 );		
+	}
+
+	SECTION( "symmetrical intersection" ) {	
+		one.position = Vector2i( 0, 0 );
+		two.position = Vector2i( 1, 0 );
+		one.size = Vector2i( 2, 2 );
+		two.size = Vector2i( 2, 2 );
+
+		REQUIRE( intersection(one, two).size.x == 1 );		
+		REQUIRE( intersection(one, two).size.y == 2 );		
+		REQUIRE( intersection(one, two).position.x == 1 );		
+		REQUIRE( intersection(one, two).position.y == 0 );	
+	}
+
+}
+
+TEST_CASE( "SimplePolygon", "[simplepolygon]" ) {
 
 	SimplePolygon<float> convexPoly;
 	convexPoly.setPointCount( 4 );
